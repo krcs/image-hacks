5d9c1622937c8673942cb2573d0763deed01356e
unknown <k.netpost@gmail.com>
2022-12-28 14:59:09 +0100

    .\jpg-exif-json.py

---
+++ jpg-exif-json.py
@@ -0,0 +1,42 @@
+#
+# JPG/JPEG Get Exif.
+#
+import os
+import argparse
+import pathlib
+import json
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', type=pathlib.Path, help="Input file.")
+args = parser.parse_args()
+
+if not args.file.exists():
+    print(f'File not found.')
+    exit(1)
+
+image = Image.open(args.file)
+exif = image.getexif()
+
+result = {
+    'Filename': str(args.file)
+}
+
+
+for id in exif:
+    tag = TAGS.get(id, id)
+    value = exif.get(id)
+
+    if isinstance(value, bytes):
+        value = value.decode()
+
+    if type(value) == int:
+        value = int(value)
+    else:
+        value = str(value)
+    result[tag] = value
+
+json_obj = json.dumps(result, indent=4)
+print(json_obj)
+
